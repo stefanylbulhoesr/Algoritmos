b6556e3680e819d5ae659e420ac04ffd5e481686
stefanylbulhoesr <slbulhoesrib@gmail.com>
2023-03-15 19:03:34 -0300

    'aula_1_algoritmos'

---
+++ python/at1.py
@@ -0,0 +1,6 @@
+
+n1 = float(input("Digite o primeiro número: "))
+n2 = float(input("Digite o segundo número: "))
+n3 = float(input("Digite o terceiro número: "))
+media = (n1 + n2 + n3)/3
+print("A média é: %.2f" %media)
---
+++ python/at2.py
@@ -0,0 +1,4 @@
+n = int(input("Digite um número: "))
+antecessor = n - 1
+sucessor = n + 1
+print("O sucessor de", n , "é", sucessor, "e o seu antecessor é", antecessor)
